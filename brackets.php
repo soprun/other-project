d618ad30a74af848b29e551060bab99ed3a0809f
Vladislav Soprun <develop@soprun.com>
2020-06-02 09:43:42 +0300

    Update brackets.php

--- brackets.php
+++ brackets.php
@@ -6,64 +6,61 @@
  */
 function isValid($input)
 {
-  static $brackets = [
-    '{' => '}',
-    '[' => ']',
-    '(' => ')',
-    '<' => '>',
-  ];
+    try {
+        static $brackets = [
+            '{' => '}',
+            '[' => ']',
+            '(' => ')',
+            '<' => '>',
+        ];
 
-  $length = mb_strlen($input);
+        $length = mb_strlen($input);
 
-  if ($length < 2) {
-    throw new Exception(
-      'An error occurred and the string length does not match the expected length.'
-    );
-  }
+        if ($length < 2) {
+            throw new Exception(
+                'An error occurred and the string length does not match the expected length.'
+            );
+        }
 
-  $stack = new SplStack();
+        $stack = new SplStack();
 
-  foreach (str_split($input) as $current => $symbol) {
-    if (array_key_exists($symbol, $brackets) === true) {
-      $stack->push($brackets[$symbol]);
-      continue;
+        foreach (str_split($input) as $current => $symbol) {
+            if (array_key_exists($symbol, $brackets) === true) {
+                $stack->push($brackets[$symbol]);
+                continue;
+            }
+
+            if ($stack->count() === 0) {
+                throw new RuntimeException('Validation error occurred, no open brackets.');
+            }
+
+            if ($stack->pop() !== $symbol) {
+                throw new LengthException(
+                    "An error occurred and the symbol {$current} requires a closed bracket of the same type."
+                );
+            }
+        }
+
+        if ($stack->count() > 0) {
+            throw new RuntimeException(
+                "There are {$stack->count()} open brackets left."
+            );
+        }
+    } catch (Exception $exception) {
+        return false;
     }
 
-    if ($stack->count() === 0) {
-      throw new RuntimeException('Validation error occurred, no open brackets.');
-    }
-
-    if ($stack->pop() !== $symbol) {
-      throw new LengthException(
-        "An error occurred and the symbol {$current} requires a closed bracket of the same type."
-      );
-    }
-  }
-
-  if ($stack->count() > 0) {
-    throw new RuntimeException(
-      "There are {$stack->count()} open brackets left."
-    );
-  }
-
-  return true;
+    return true;
 }
 
 echo PHP_EOL;
 
-try {
-  var_dump(isValid('')); // return false
-  var_dump(isValid('()')); // return true
-//  var_dump(isValid('(){}')); // return true
-//  var_dump(isValid('(){}[]')); // return true
+// false
+echo PHP_EOL . (isValid(null) ? 'True!.' : 'False!.');
+echo PHP_EOL . (isValid('{') ? 'True!.' : 'False!.');
+echo PHP_EOL . (isValid('}') ? 'True!.' : 'False!.');
+echo PHP_EOL . (isValid('}{') ? 'True!.' : 'False!.');
 
-  echo PHP_EOL;
-//  var_dump(isValid('}')); // return false
-//  var_dump(isValid('}{')); // return false
-//  var_dump(isValid('{{()}}')); // return false
-//  var_dump(isValid('(}{((((>){[{{{([])]<')); // return false
-} catch (Exception $exception) {
-  echo $exception->getMessage();
-}
 
-echo '';
+// true
+echo isValid('{}') ? 'True!.' : 'False!.';
